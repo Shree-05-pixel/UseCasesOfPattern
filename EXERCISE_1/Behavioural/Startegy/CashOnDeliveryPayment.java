2f20b4a1efae57b54260f7d89e96ce8ceef55657
Shree-05-pixel <shobanashree75@gmail.com>
2025-10-01 23:16:19 +0530

    Update CashOnDeliveryPayment.java

--- EXERCISE_1/Behavioural/Startegy/CashOnDeliveryPayment.java
+++ EXERCISE_1/Behavioural/Startegy/CashOnDeliveryPayment.java
@@ -1,8 +1,9 @@
-package Startegy;
+package EXERCISE_1.Behavioural.Startegy;
 
 public class CashOnDeliveryPayment implements PaymentStrategy {
     @Override
     public void pay(double amount) {
         System.out.println("Order placed with Cash on Delivery. Pay Rs." + amount + " on delivery.");
     }
+
 }
