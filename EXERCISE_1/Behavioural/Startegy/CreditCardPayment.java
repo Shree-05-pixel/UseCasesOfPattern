7247231555c70f3d23758f2666624771ec70ab2b
Shree-05-pixel <shobanashree75@gmail.com>
2025-10-01 23:17:05 +0530

    Update CreditCardPayment.java

--- EXERCISE_1/Behavioural/Startegy/CreditCardPayment.java
+++ EXERCISE_1/Behavioural/Startegy/CreditCardPayment.java
@@ -1,6 +1,12 @@
-package Startegy;
+package EXERCISE_1.Behavioural.Startegy;
 
+import java.util.Scanner;
 public class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+
+    public CreditCardPayment(String cardNumber) {   
+        this.cardNumber = cardNumber;
+    }
     @Override
     public void pay(double amount) {
         Scanner sc = new Scanner(System.in);
