d8a3dade182768d0ebd224e7a72612ee4fa01278
Shree-05-pixel <shobanashree75@gmail.com>
2025-10-01 23:20:44 +0530

    Update UPIPayment.java

--- EXERCISE_1/Behavioural/Startegy/UPIPayment.java
+++ EXERCISE_1/Behavioural/Startegy/UPIPayment.java
@@ -1,11 +1,17 @@
-package Startegy;
+package EXERCISE_1.Behavioural.Startegy;
+ import java.util.Scanner;
 
 public class UPIPayment implements PaymentStrategy {
+    private String upiId;
+
+    public UPIPayment(String upiId) {   //  added constructor
+        this.upiId = upiId;
+    }
     @Override
     public void pay(double amount) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter UPI ID: ");
         String upiId = sc.nextLine();
-        System.out.println("✅ Payment of Rs." + amount + " done via UPI (" + upiId + ")");
+        System.out.println(" Payment of Rs." + amount + " done via UPI (" + upiId + ")");
     }
 }
