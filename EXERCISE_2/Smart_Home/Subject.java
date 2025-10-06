1cfe94643db874c88bb204ba1cc82d7eb18dec0a
Shree-05-pixel <shobanashree75@gmail.com>
2025-10-06 12:35:44 +0530

    Update Subject.java

--- EXERCISE_2/Smart_Home/Subject.java
+++ EXERCISE_2/Smart_Home/Subject.java
@@ -1,7 +1,9 @@
-package Smart_Home;
+package EXERCISE_2.Smart_Home;
+
 
 public interface Subject {
     void addObserver(Observer observer);
     void removeObserver(Observer observer);
     void notifyObservers(String eventType, Object data);
 }
+
